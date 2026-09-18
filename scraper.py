c453c3c9bf502e0ccde95d98332ddb7f17cb1c72
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-18 09:10:07 +0600

    fix

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 # 1. Opening the local HTML file in Python
 with open("sample.html", "r", encoding="utf-8") as file:
-html_content = file.read()
+ html_content = file.read()
 
 # 2. Parsing the HTML with BeautifulSoup
 soup = BeautifulSoup(html_content, "html.parser")
@@ -27,8 +27,8 @@ print("--- 3. Extracting information for all books using a loop ---")
 all_books = soup.find_all("div", class_="book")
 
 for book in all_books:
-title = book.find("h2", class_="book-title").text
-author = book.find("p", class_="author").text
-price = book.find("p", class_="price").text
+ title = book.find("h2", class_="book-title").text
+ author = book.find("p", class_="author").text
+ price = book.find("p", class_="price").text
 
 print(f"Book: {title} | {author} | {price}")
